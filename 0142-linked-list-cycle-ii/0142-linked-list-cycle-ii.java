6ae8c38fabf8d2879484f40429103887a063e38c
Anuj Tiwari <tiwarianuj940@gmail.com>
2026-07-18 20:35:57 +0530

    Time: 0 ms (100%), Space: 46.8 MB (13.62%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,24 +11,20 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-
     ListNode slow = head;
     ListNode fast = head;
-
     while (fast != null && fast.next != null) {
-
         slow = slow.next;
         fast = fast.next.next;
-
         if (slow == fast) {
-
-            ListNode b = head;
-
-            while (b != slow) {
-                b = b.next;
+        ListNode temp = head;
+//when slow equal fast means cycle/loop then took b at head now slow will start where is it means where slow=fast and b from head and the node where they will meet that definitely will be 1st node of that cycle
+            while (temp != slow) {
+                temp = temp.next;
                 slow = slow.next;
-            }  return b;
+            }  return temp;
         }
     }
  return null;
-}  }
+ }
+}
